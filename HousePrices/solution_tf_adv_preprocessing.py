880aaad7d4881e1b2975023e835a8a41dcf3df76
Luca Begatti <luca.begatti93@gmail.com>
2023-11-29 23:33:27 +0100

    - completed with both Deep Learning (DL) and RandomForest (RF) tuning. - it looks like the RF is better for predicting house prices...

--- HousePrices/solution_tf_adv_preprocessing.py
+++ HousePrices/solution_tf_adv_preprocessing.py
@@ -6,8 +6,14 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, RandomizedSearchCV
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+import tensorflow as tf
+from keras import Sequential
+from keras import layers
+from keras.layers import Dense, Dropout
 
 # set the directory
 os.chdir('/Users/lucabegatti/Desktop/Pycharm/Kaggle/HousePrices')
@@ -59,7 +65,7 @@ plt.show()
 total = dataset.isnull().sum().sort_values(ascending=False)
 percent = (dataset.isnull().sum() / dataset.isnull().count()).sort_values(ascending=False)
 missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
-missing_data.head(36)
+# missing_data.head(36)
 
 dataset = dataset.drop(['PoolQC', 'MiscFeature', 'Alley', 'Fence', 'MasVnrType', 'FireplaceQu',
                         'LotFrontage'], axis=1)
@@ -72,12 +78,12 @@ dataset = encoder.fit_transform(dataset)
 simple_impute = SimpleImputer(strategy='mean', missing_values=np.nan)
 dataset2 = simple_impute.fit_transform(dataset)
 dataset_final = pd.DataFrame(dataset2, columns=dataset.columns)
-dataset_final.tail()
+# dataset_final.tail()
 
 # separate dataset test
 dataset_test_final = dataset_final[dataset_final['col'] == 0]
 dataset_test_final = dataset_test_final.drop(['SalePrice'], axis=1)
-dataset_test_final.tail()
+# dataset_test_final.tail()
 
 # train dataset split
 dataset_train_final = dataset_final[dataset_final['col'] == 1]
@@ -97,7 +103,104 @@ corrmat = X.corr()
 f, ax = plt.subplots(figsize=(10, 10))
 sns.heatmap(corrmat, vmax=1, cbar=True, square=True)
 
-
 # create a RandomForest
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
 
-# hyperparameter Tuning
+feature_importances = model.feature_importances_
+
+importance_df = pd.DataFrame({'Feature': X_train.columns, 'Importance': feature_importances})
+importance_df = importance_df.sort_values(by='Importance', ascending=False)
+
+# remove the features with little importance
+X_train = X_train.drop(['col', 'MiscVal', '3SsnPorch', 'LowQualFinSF', 'PoolArea', 'EnclosedPorch', 'KitchenAbvGr',
+                        'BsmtFullBath', 'BsmtFinSF2', 'HalfBath'], axis=1)
+X_test = X_test.drop(['col', 'MiscVal', '3SsnPorch', 'LowQualFinSF', 'PoolArea', 'EnclosedPorch', 'KitchenAbvGr',
+                      'BsmtFullBath', 'BsmtFinSF2', 'HalfBath'], axis=1)
+
+dataset_test_final = dataset_test_final.drop(
+    ['col', 'MiscVal', '3SsnPorch', 'LowQualFinSF', 'PoolArea', 'EnclosedPorch', 'KitchenAbvGr',
+     'BsmtFullBath', 'BsmtFinSF2', 'HalfBath'], axis=1)
+
+# Finding the best model via Deep Learning (Hyperparameter Tuning)
+model_tf = Sequential()
+model_tf.add(Dense(128, input_dim=X_train.shape[1], activation='relu'))
+model_tf.add(Dropout(0.2))
+model_tf.add(Dense(64, activation='relu'))
+model_tf.add(Dense(1, activation='linear'))  # linear activation for regression
+
+model_tf.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+
+best_model = None
+best_mse = float('inf')
+
+for epochs in [50, 100, 200]:
+    for batch_size in [32, 64, 128]:
+
+        # train the model on the training data
+        history = model_tf.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.2, verbose=0)
+
+        # evaluate the model on the validation data
+        val_mse = mean_squared_error(y_test, model_tf.predict(X_test))
+
+        # check if the model has the best performance
+        if val_mse < best_mse:
+            best_model = model_tf
+            best_mse = val_mse
+
+print("Best Model MSE:", best_mse)
+
+test_mse = mean_squared_error(y_test, best_model.predict(X_test))
+print("Test MSE:", test_mse)
+
+# use the best model to make prediction on the test data
+dl_predictions = best_model.predict(dataset_test_final)
+dl_predictions_df = pd.DataFrame({'Id': range(1461, 1461 + len(dl_predictions)),
+                                  'SalePrice': dl_predictions.flatten()})
+
+# Apply RF Regression and doing hyperparameter tuning
+RF = RandomForestRegressor()
+RF.fit(X_train, y_train)
+scores = cross_val_score(RF, X_train, y_train, cv=10)
+scores.mean()
+
+params = {"n_estimators": [100, 200, 300, 400, 500, 1000],  # number of trees in the forest
+          "max_features": ['sqrt', 'log2'],  # how to decide max number of features when looking for the best split
+          "max_depth": [None, 10, 20, 30, 40, 50, 100],  # max depth of the tree
+          "min_samples_split": [2, 5, 10, 20, 50],  # min num of samples required to split an internal node
+          "min_samples_leaf": [1, 2, 4, 8],  # min number of samples required to be at a leaf node
+          "bootstrap": [True, False]  # whether to bootstrap samples when building trees
+          }
+
+random_search = RandomizedSearchCV(
+    estimator=RF,
+    param_distributions=params,
+    n_iter=10,  # number of random combinations to try
+    cv=10,  # number of cv folds
+    scoring="neg_mean_squared_error",  # scoring metrics
+    n_jobs=-1,  # use all cpu cores
+    random_state=42
+)
+
+random_search.fit(X_train, y_train)
+best_params = random_search.best_params_
+print("\nBest Hyperparameters:", best_params)
+
+best_model_RF = random_search.best_estimator_
+print("\nBest model :", best_model_RF)
+
+# initalize a new RF with the optimal parameters
+RF_final = RandomForestRegressor(max_depth=20, max_features='sqrt', n_estimators=400, min_samples_split=5,
+                                 min_samples_leaf=1, bootstrap=False)
+RF_final.fit(X_train, y_train)
+scores_final = cross_val_score(RF_final, X_train, y_train, cv=10)
+print(scores_final)
+print("\n mean of the Scores :", scores_final.mean())
+
+# predictions
+
+rf_predictions = RF_final.predict(dataset_test_final)
+rf_predictions_df = pd.DataFrame({"Id": range(1461, 1461 + len(rf_predictions)),
+                                  "SalePrice": rf_predictions
+                                  })
+rf_predictions_df.to_csv('rf_predictions.csv', index=False)
